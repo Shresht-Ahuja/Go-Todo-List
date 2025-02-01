0058285fa5510590b18fc6f8e16c830296b7ea19
Shresht-Ahuja <shreshtahuja.in@gmail.com>
2025-02-01 22:00:13 +0530

    updated storage.go

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const file = "todos.json"
+
 func main() {
 	var todos Todos
 
--- storage.go
+++ storage.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
-)
-func saveTodosToFile(todos Todos, filename string) error {
-	data, err := json.MarshalIndent(todos, "", "  ")
-	if err != nil {
-		return fmt.Errorf("error in todos: %w", err)
+	"os"
+	)
+	
+	type Storage[T any] struct {
+		FileName string
 	}
-	err = ioutil.WriteFile(filename, data, 0644)
-	if err != nil {
-		return fmt.Errorf("error writing to file: %w", err)
+	
+	func NewStorage[T any](fileName string) *Storage[T] {
+		return &Storage[T]{FileName: fileName}
 	}
+	
+	func (s *Storage[T]) Save(data T) error {
+		fileData, err := json.MarshalIndent(data, "", "")
 
-	return nil
-}
+		if err != nil {
+			return err
+	}
+	return os.WriteFile(s.FileName, fileData, 0644)
+	}
+	
+	func (s *Storage[T]) Load(data *T) error {
+		fileData, err := os.ReadFile(s.FileName)
+		if err != nil {
+		 return err
+		}
+	
+	return json.Unmarshal(fileData, data)
+	}
