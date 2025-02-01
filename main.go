4af2aedfae35b1e6bcc5b80c7ec5020817438325
Shresht-Ahuja <shreshtahuja.in@gmail.com>
2025-02-01 22:29:50 +0530

    loads and stores data from json

--- main.go
+++ main.go
@@ -5,12 +5,10 @@ import "fmt"
 const file = "todos.json"
 
 func main() {
-	var todos Todos
-
-	todos.add("Buy milk")
-	todos.add("Build a CLI app")
-
+	todos := Todos{}
+	storage := NewStorage[Todos]("todos.json")
+	storage.Load(&todos)
 	fmt.Println("Your Todo List:")
-	todos.toggle(0)
 	todos.print()
+	storage.Save(todos)
 }
